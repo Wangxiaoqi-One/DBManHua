ce05a58e9377a5e73718a62209e0761cc3928c85
WangXiaoQi <WangXiaoQi@gmail.com>
2016-03-03 16:19:59 +0800

    first

---
+++ HAHAManhua/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HAHAManhua
+//
+//  Created by scjy on 16/3/3.
+//  Copyright © 2016年 WangXiaoQi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
